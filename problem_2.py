2aeefd9000e0652700fdcbe39084a85786ea756f
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2020-12-27 15:29:09 +0400

    Changed

--- problem_2.py
+++ problem_2.py
@@ -1,7 +1,7 @@
 date = int(input("Enter date: "))
 print("Yo entered - ",date)
 if date > 0 and date < 2022:
-    if(date % 100 == 0):
+    if date % 100 == 0:
         print("It is the  ", date // 100, "st century ", sep="")
     else:
         print("It is the  ",date//100+1, "st century ", sep = "")

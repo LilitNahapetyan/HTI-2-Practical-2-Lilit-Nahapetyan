632bb547bdc9a297ed0d5aee8c322a1f3625ef49
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2021-01-06 00:27:18 +0400

    After change

--- Homework_2/is_lucky.py
+++ Homework_2/is_lucky.py
@@ -4,9 +4,9 @@ num = [int(i) for i in number]
 
 half_sum = 0
 sum = 0
-for i in range(0, int(len(number)//2)):
+for i in range(len(number)//2):
     half_sum = half_sum + num[i]
-for i in range(0, int(len(number))):
+for i in range(len(number)):
     sum = sum + num[i]
 if half_sum == sum/2:
     print("Yes")

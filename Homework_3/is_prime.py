3e7103ddf4c928a618d80d7b86de34c6047a2fd6
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2021-01-16 23:49:49 +0400

    Change

--- Homework_3/is_prime.py
+++ Homework_3/is_prime.py
@@ -1,13 +1,14 @@
 def is_prime(num):
-    x, sum = 2, 0
-    while x <= num - 1:
-        if num % x == 0:
-            print("No")
-            break
-        x += 1
+    if num < 2:
+        print("No")
     else:
-        print("Yes")
-
-
+        x, sum = 2, 0
+        while x <= num - 1:
+            if num % x == 0:
+                print("No")
+                break
+            x += 1
+        else:
+            print("Yes")
 num = int(input("Num: "))
 is_prime(num)

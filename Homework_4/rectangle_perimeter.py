d144f860ceea5a36d22d8a66212e0722ca8883ae
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2021-01-24 22:00:37 +0400

    Homework

--- Homework_4/rectangle_perimeter.py
+++ Homework_4/rectangle_perimeter.py
@@ -6,7 +6,7 @@ def rec_perimeter(num):
     perimeter = segment_length(num[0], num[1], num[2], num[3]) \
                 + segment_length(num[2], num[3], num[4], num[5]) \
                 + segment_length(num[4], num[5], num[6], num[7]) \
-                + segment_length(num[6], num[7], num[1], num[2])
+                + segment_length(num[6], num[7], num[0], num[1])
     return perimeter
 
 

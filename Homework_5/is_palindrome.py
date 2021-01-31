2c98713bd0a2ff48a9d55692bcaf5fb6056e010e
LilitNahapetyan <nahapetyanlilit01@gmail.com>
2021-02-01 00:44:56 +0400

    Homework

--- Homework_5/is_palindrome.py
+++ Homework_5/is_palindrome.py
@@ -2,7 +2,7 @@ def is_palindrome(x):
     if len(x) == 1 or len(x) == 0:
         return True
     if x[0] == x[-1]:
-        return is_palindrome(x[len(x) - 1])
+        return is_palindrome(x[1:-1])
     return False
 
 value = input("Input any string: ")
--- Homework_5/roman_to_integer.py
+++ Homework_5/roman_to_integer.py
@@ -1,16 +1,24 @@
 def roman_to_integer(rom):
     dict_rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    sum = 0
+
+    if len(rom) == 1:
+        return dict_rom[rom[0]]
+
+    sum_el = 0
     i = 0
     while i < (len(rom) - 1):
         if dict_rom[rom[i]] < dict_rom[rom[i + 1]]:
-            sum += dict_rom[rom[i + 1]] - dict_rom[rom[i]]
+            sum_el += dict_rom[rom[i + 1]] - dict_rom[rom[i]]
             i += 2
         else:
-            sum += dict_rom[rom[i]]
+            sum_el += dict_rom[rom[i]]
             i += 1
-    return sum
+
+    if dict_rom[rom[-1]] <= dict_rom[rom[-2]]:
+            sum_el += dict_rom[rom[-1]]
+
+    return sum_el
+
 rom_num = input("Input Roman Number: ").upper()
 print(roman_to_integer(rom_num))
-
 
